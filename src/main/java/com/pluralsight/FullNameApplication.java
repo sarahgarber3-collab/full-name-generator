a864aa5b1b29496b5ac1875a48c99fce281af2cd
sarahgarber3@gmail.com <sarahgarber3@gmail.com>
2026-04-13 13:09:48 -0400

    Exercise one

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -1,4 +1,30 @@
 package com.pluralsight;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class FullNameApplication {
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Please enter your first name: ");
+        String firstName = scan.nextLine();
+
+        System.out.print("please enter your middle name: ");
+        String middleName = scan.nextLine();
+
+        System.out.print("Please enter your last name: ");
+        String lastName = scan.nextLine();
+
+        System.out.print("Please enter a suffix if you have one: ");
+        String suffix = scan.nextLine();
+
+        String fullName = firstName + middleName + suffix + lastName;
+        System.out.println(fullName);
+
+
+
+    }
 }
